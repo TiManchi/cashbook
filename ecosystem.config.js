6465b6b25d649d2de2631082c36533c33c1e90a8
TiManchi <651511823@qq.com>
2022-02-12 11:13:23 +0800

    cashbook

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'juejue-vite-h5',
+      script: 'juejue-vite-h5-server.js'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '121.41.207.81',
+      ref: 'origin/main',
+      repo: 'git@github.com:TiManchi/cashbook.git',
+      path: '/usr/workspace/juejue-vite-h5',
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
